3f139c3494e0082b1dbd7af9abd28cfc62cfa212
João Henrique Delucca Barros <jhdbarros@hotmail.com>
2020-10-30 00:09:25 -0300

    add gasto detalhado edita gasto e centro custo

--- src/controller/GastoDetalhadoController.js
+++ src/controller/GastoDetalhadoController.js
@@ -22,12 +22,14 @@ module.exports = {
         const {gasto_id} = request.params;
 
         const users = await connection('usuarios').where('id', usr_id).select('*').first();
+        
 
         if (!users) {
             return response.status(401).json({ error: 'Operation not permited' });
         }
 
         const gastos = await connection('gastos_detalhados').where('gasto_id', gasto_id).select('*');   
+       
 
         if (!gastos) {
             return response.status(204).json('No contents');
@@ -65,8 +67,12 @@ module.exports = {
         const usr_id = request.headers.authorization;
         const { centro_custo_id, gasto_id } = request.query;
 
-        const user = await connection('usuarios')
-            .where('id', usr_id)
+        const trx = await connection.transaction();
+
+        try {
+
+        const user = await trx('usuarios')
+            .where('id', usr_id)            
             .select('*')
             .first();
 
@@ -74,14 +80,54 @@ module.exports = {
             return response.status(401).json({ error: 'Operation not permited' });
         }
 
-        const [id] = await connection('gastos_detalhados').insert({
+        const [id] = await trx('gastos_detalhados').insert({
             gasto_id,
             centro_custo_id,
             titulo,
             valor
         });
 
+        const gasto = await trx('gastos').where('id',gasto_id).select('*').first();
+
+        if (!gasto) {
+            return response.status(401).json({ error: 'Operation not permited' });
+        }
+
+        await trx('gastos').where('id',gasto_id).update({
+            valorGasto: gasto.valorGasto + valor,
+        });
+
+        const centro_custo = await trx('centro_custo').where('id',centro_custo_id).select('*').first();
+
+        if (!centro_custo) {
+            return response.status(401).json({ error: 'Operation not permited' });
+        }
+
+        console.log(centro_custo.tipo);
+
+        if(centro_custo.tipo === 'CC') {
+            centro_custo.valorCusto = centro_custo.valorCusto - valor;
+        }else{
+            centro_custo.valorCusto = centro_custo.valorCusto + valor;    
+        }
+
+        await trx('centro_custo').where('id',centro_custo_id).update({
+            valorCusto: centro_custo.valorCusto,
+        });
+
+
+        await trx.commit();
+
         return response.json({ id });
+
+    } catch (err) {
+        await trx.rollback();
+        console.log(err);
+
+        return response.status(400).json({
+            error: 'Unexpected erro while creating new class'
+        })
+    }
 
     },
 
